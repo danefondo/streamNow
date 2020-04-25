31e662e53384e9c0e0a7ee36ca44289ba7d37160
Robert Unyx <robertunyx@gmail.com>
2020-04-25 21:46:10 +0300

    use right backend

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,1 +1,1 @@
-export const BASE_PATH = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "http://eeter.tv";
+export const BASE_PATH = process.env.NODE_ENV === "development" ? "http://localhost:3000" : window.location.origin;
