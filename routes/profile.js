372a614960bcdec2650043d4cb48f7cdbcc503bd
Robert Unyx <robertunyx@gmail.com>
2020-04-27 15:01:45 +0300

    only check if authenticated

--- routes/profile.js
+++ routes/profile.js
@@ -1,33 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const assert = require('assert');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const { validationResult } = require('express-validator');
-mongoose.Promise = Promise;
+const auth = require('../config/auth')
 
 const router = express.Router();
 
-let User = require('../models/user');
-const validator = require('../controller/validator')
-
 const streamController = require('../controller/stream');
 
-// allow user to proceed to next middleware even if not logged in
-const checkAuthenticated = passport.authenticate('jwt', { session: false })
-router.get('/user/:userId', checkAuthenticated, streamController.getStreamer);
+router.get('/user/:userId', auth.checkAuthenticated, streamController.getStreamer);
 
 
-/*====== Access control  ======*/
-function ensureAuthenticated(req, res, next){
-  if(req.isAuthenticated()){
-  	console.log("Authentication successful.");
-    return next();
-  } else {
-  	console.log("Authentication failed.");
-    res.redirect(302, '/');
-  }
-}
 
 module.exports = router;
