9fc5723a9f1e5fc209262a7f385e54f58bea0964
Robert Unyx <robertunyx@gmail.com>
2020-04-28 16:15:39 +0300

    change

--- client/src/pages/Privacy.vue
+++ client/src/pages/Privacy.vue
@@ -1,5 +1,106 @@
 <template>
+  <div class="footer_page_container">
     <AboutNav />
+    <div class="footer_page">
+      <div class="textTitle">Eeter.tv privaatsuseeskiri</div>
+      <div class="textSection">
+        <span class="textStyle">28.04.2020</span>
+      </div>
+
+      <div class="textSection">
+        <span class="textStyle">Palun tutvu ka üldtingimuste ja mõistetega meie
+          <a
+            href="https://www.eeter.tv/terms"
+          >kasutustingimuste</a> lehel.
+          <br />Andmete töötlemise põhimõtted kehtivad Eeter.tv Kasutajatele.
+          <br />
+          <br />Tere tulemast Eeter.tv veebilehele. Kasutustingimused kehtivad kõikidele Eeter.tv lehe külastajatele ja Kasutajatele.
+        </span>
+      </div>
+
+      <div class="textTitle">Andmete kogumine:</div>
+      <div class="textSection">
+        <span class="textStyle">
+          Kasutajaks registreerimisel jagunevad Kasutaja poolt esitamisele kuuluvad andmed kohustuslikeks ning vabatahtlikult edastatavateks andmeteks.
+          <br />
+          <br />Kasutaja registreerimisel andmete sisestamist koheldakse kui nõusolekut oma kontaktandmete kasutamiseks turunduse eesmärgil. Kasutajale edastatakse turundusliku iseloomuga teadaandeid tema poolt Eeter.tv -le antud kontaktandmeid kasutades.
+          <br />
+          <br />Kasutajal on igal ajal õigus otseturunduslikel eesmärkidel saadetavate teadaannete saamisest loobuda saates vastavasisulise e-kirja meiliaadressile
+          <a
+            href="mailto:tere@eeter.tv"
+          >tere@eeter.tv</a>.
+          <br />
+          <br />Tellija andmete vastutav töötleja on eeter.tv.
+        </span>
+      </div>
+
+      <div class="textTitle">Andmete vastutav ja volitatud töötleja:</div>
+      <div class="textSection">
+        <span
+          class="textStyle"
+        >Ekspress Meedia võib anda andmete kasutamiseks õiguse volitatud töötlejatele. Volitatud töötlejad on Eeter.tv koostööpartnerid kes töötlevad andmeid muu hulgas teenuse osutamise eesmärgil, Kasutajate korrastatud andmebaasi pidamiseks, teeninduslikul ja turunduslikul eesmärgil, v.a siis, kui Tellija on otsustanud teisiti või hiljem vastava nõusoleku tagasi võtnud, teavitades sellest kirjalikku taasesitamist võimaldavas vormis ja täitmaks õigusaktidest tulenevaid kohustusi (nt andmete edastamine uurimisasutusele). Volitatud töötleja võib andmeid töödelda üksnes Eeter.tv poolt antud konkreetsete ülesannete täitmiseks.</span>
+      </div>
+
+      <div class="textTitle">Andmete säilitamise aeg:</div>
+      <div class="textSection">
+        <span
+          class="textStyle"
+        >Eeter.tv säilitab andmeid nii kaua, kui neid on vaja andmete kasutamise eesmärgi saavutamiseks või seadusest tulenevate nõude aegumistähtaegade saabumiseni ja seadusest tuleneva dokumendi säilitustähtaja saabumiseni (vastavalt käibemaksuseadusele ning raamatupidamise seadusele 7 aastat).</span>
+      </div>
+
+      <div class="textTitle">Kasutaja õigused andmete kasutamisega seoses:</div>
+      <div class="textSection">
+        <span
+          class="textStyle"
+        >Kasutajal on õigus saada Eeter.tv -lt teavet enda andmete kasutamise kohta seaduses sätestatud korras ja ulatuses; Kasutajal on õigus nõuda Eeter.tv -lt enda andmete kasutamise lõpetamist ning andmete parandamist, sulgemist ja kustutamist; Kasutajal on õigus nõustuda või keelata oma andmete kasutamine otseturunduseks või turunduslikel eesmärkidel.</span>
+      </div>
+
+      <div class="textTitle">Küpsiste kasutamise põhimõtted:</div>
+      <div class="textSection">
+        <span
+          class="textStyle"
+        >Kasutajate paremaks teenindamiseks kasutab Eeter.tv veebileht küpsiseid ehk cookie’sid. Eeter.tv -le kuuluvad domeenid võivad muuhulgas sisaldada elementi, mis salvestavad küpsiseid kolmandate isikute nimel.</span>
+      </div>
+
+      <div class="textTitle">Küpsistest keeldumine:</div>
+      <div class="textSection">
+        <span class="textStyle">
+          Kasutajal on õigus keelduda küpsiste salvestamisest arvutisse. Vastava soovi korral peab Kasutaja muutma oma veebilehitseja seadeid.
+          <br />
+          <br />Eri tüüpi veebilehitsejad kasutavad küpsiste keelamiseks erinevaid meetodeid. Täpsem informatsioon on kuvatud veebilehel aki.ee/et/kupsised.
+          <br />
+          <br />Küpsiste blokeerimisel peab Kasutaja arvestama, et kõik veebilehe funktsioonid ei pruugi pärast küpsiste blokeerimist enam Kasutajale kättesaadavad olla.
+        </span>
+      </div>
+
+      <div class="textTitle">Mis küpsis ehk cookie?</div>
+      <div class="textSection">
+        <span class="textStyle">
+          Küpsis on tekstifail, mille saadavad ja salvestavad Kasutaja arvutisse veebilehed, mida kasutaja külastab. Küpsis salvestatakse Kasutaja veebilehitseja failikataloogis. Juhul kui Kasutaja on veebilehte varem külastanud, loeb veebilehitseja küpsist ning edastab vastava teabe veebilehele või elemendile, mis küpsise algul salvestas. Lisateave küpsiste kohta on kättesaadav veebilehel www.aki.ee/et/kupsised.
+          <br />
+          <br />Küpsised võimaldavad jälgida veebilehe kasutusstatistikat, rubriikide populaarsust ning muid veebilehel tehtavaid toiminguid. Küpsisest saadavat teavet kasutatakse veebilehe kasutusmugavuse ja sisu parandamise eesmärgil.
+        </span>
+      </div>
+
+      <div class="textTitle">Veebilehel kasutatavate küpsiste tüübid:</div>
+      <div class="textSection">
+        <span class="textStyle">
+          Püsiküpsised on hädavajalikud veebilehel ringi liikumiseks ja selle funktsioonide kasutamiseks. Püsiküpsiste puudumisel ei saaks Kasutaja kasutada kõiki veebilehe funktsioone.
+          <br />
+          <br />Sessiooniküpsised võimaldavad veebilehel meelde jätta Kasutaja poolt tehtud varasemaid valikud (näiteks kasutajanimi, keelevalik jne) ning seeläbi pakkuda tõhusamaid ja isikupärasemaid funktsioone.
+          <br />
+          <br />Jälgimisküpsised koguvad andmeid Kasutaja käitumisest veebilehel. Jälgimisküpsisest saadav informatsioon võimaldab tõsta veebilehe kasutusmugavust.
+          <br />
+          <br />Reklaamküpsised koguvad andmeid Kasutaja sirvimisharjumuste kohta, mis omakorda võimaldab veebilehel esitada Kasutaja eelistustega kooskõlas olevat reklaamisisu. Lisaks võimaldab vastav küpsiseliik mõõta reklaamikampaania tõhusust.
+          <br />
+          <br />Kõikide küsimuste ja pretensioonide korral palun kirjutada
+          <a
+            href="mailto:tere@eeter.tv"
+          >tere@eeter.tv</a>.
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,13 +109,59 @@ import AboutNav from "../components/AboutNav";
 export default {
   name: "Privacy",
   components: {
-      AboutNav
+    AboutNav
   }
 };
 </script>
 
 
 <style>
+.textTitle {
+  font-size: 11pt;
+  font-family: Arial;
+  color: rgb(0, 0, 0);
+  background-color: transparent;
+  font-weight: 700;
+  font-style: normal;
+  font-variant: normal;
+  text-decoration: none;
+  vertical-align: baseline;
+  white-space: pre-wrap;
+  margin-bottom: 6px;
+}
+.textStyle {
+  font-size: 11pt;
+  font-family: Arial;
+  color: rgb(0, 0, 0);
+  background-color: transparent;
+  font-style: normal;
+  font-variant: normal;
+  text-decoration: none;
+  vertical-align: baseline;
+  white-space: pre-wrap;
+  line-height: 1.38;
+  margin-top: 0pt;
+  margin-bottom: 0pt;
+}
+
+.textSection {
+    margin-bottom: 30px;
+}
+
+.footer_page_container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  width: 100%;
+  font-family: "Trebuchet MS", sans-serif;
+}
+.footer_page {
+  padding: 30px 0px;
+  padding-bottom: 40px;
+  font-size: 18px !important;
+  max-width: 600px;
+  margin: 0 auto;
+}
 /*! CSS Used from: https://www.eeter.tv/css/navigation.css */
 .topNavigationLink__topBar {
   border-radius: 4px;
--- client/src/pages/Terms.vue
+++ client/src/pages/Terms.vue
@@ -1,5 +1,68 @@
 <template>
+  <div class="footer_page_container">
     <AboutNav />
+    <div class="footer_page">
+      <div class="textTitle">Eeter.tv kasutustingimused</div>
+      <div class="textSection">
+        <span class="textStyle">28.04.2020</span>
+      </div>
+
+      <div class="textTitle"></div>Mõisted
+      <div class="textSection">
+        <span class="textStyle">
+          Eeter.tv - Meie, tere@eeter.tv
+          <br />Kasutustingimused - käesolevad tingimused veebilehe kasutamiseks, sisu üleslaadimiseks, toetuste kogumiseks.
+          <br />Kasutaja - Eeter.tv keskkonna kasutaja.
+          <br />Looja - Kasutaja, kes lisab Eeter.tv keskkonda videosisu, ja/või kogub sellelt toetuseid.
+          <br />Toetaja - Kasutaja, kes annetab Eeter.tv platvormi vahendusel Loojatele raha.
+          <br />Pool - Eeter.tv või Kasutaja, koos nimetatud Pooled.
+          <br />Sisu - Eeter.tv keskkonda saadetud video, pilt, või kirjutis.
+          <br />Toetus - Kasutaja tehtud annetus.
+        </span>
+      </div>
+
+      <div class="textSection">
+        <span class="textStyle">
+          Tere tulemast Eeter.tv veebilehele. Kasutustingimused kehtivad kõikidele Kasutajatele.
+          <br />
+          <br />Eeter.tv -l on õigus Üldtingimusi ühepoolselt muuta. Üldtingimuste muudatusest teavitab Eeter.tv Kasutajaid kodulehel ja e-posti teel.
+          <br />
+          <br />Eeter.tv ei oma juriidilist vastutust kasutajate poolt sisestatud Sisu eest.
+          <br />
+          <br />Sisu meile saatmisel ja avaldamisel palun arvesta, et me eeldame Looja autorsust või õiguste omamist, mis lubab seda teost üldsusele kättesaadavaks teha. Teose meie platvormi lisamisel annab Looja Eeter.tv platvormile tasuta õiguse teose vabaks kasutamiseks mistahes piiranguteta, sealhulgas, kuid mitte ainult, tähtaja, õiguste mahu, nende kasutamise vormi, viisi, tehnilise vahendi või territooriumi osas.
+          <br />
+          <br />Kuna Eeter.tv vaid koondab kolmandate platvormide Sisu, on Loojal täielik kontroll oma lisatud sisu üle just läbi nende kolmandate platvormide.
+          <br />
+          <br />Kolmandatele osapooltele on Eeter.tv keskkonnas leiduva Sisu avaldamine, dubleerimine või muul viisil kasutamine ilma ühe Sisu kasutusõigust omava Poole kirjaliku loata keelatud.
+          <br />
+          <br />Kui Sa oled Eeter.tv keskkonnas asuva Sisu autor või õiguste omaja ja Sa ise ei ole seda Sisu meile saatnud ning soovid, et see Sisu ei oleks üldsusele kättesaadav, siis saada palun meile e-mail aadressile:
+          <a
+            href="mailto:tere@eeter.tv"
+          >tere@eeter.tv</a>.
+          <br />
+          <br />Eeter.tv on antud tehingute puhul vahendaja rollis ja tänase seisuga Eeter.tv tehtud Toetustelt teenustasu ei võta. Kõik saadud tulud kantakse üle Loojatele. Toetuse valuuta on Euro. Eeter.tv platvormil tehtud annetused (toetused) ei ole tagastatavad.
+          <br />
+          <br />Toetusega kaasneb siiski 3%-ne maksevahendajate tasu, mis ei sõltu paraku meist ja on otsene kulu igalt makselt. Minimaalne tasu ühe ostu kohta on 18 senti. Maksevahendajate tasule lisandub käibemaks.
+          <br />
+          <br />Kuna Sisu on kõikidele vaatajatele tasuta kättesaadav ning toetusi tehakse vaid oma vabal soovil ja valikul, siis ka eraisikust Loojatel ei teki maksukohustust, sest eraisikute vahelisi annetusi ei maksustata. Tulenevalt eeltoodust ei teki Eeter.tv maksukohustust saadud tulude edasikandmisel. Kui Toetajaks on ettevõte, langeb sellega seotud juriidiline vastutus Toetajale.
+          <br />
+          <br />Toetajal ei ole võimalik tehtud toetust oma maksustatavast tulust maha arvata. Sihtasutustele ja MTÜ-de toetamisel rakenduvad eritingimused.
+          <br />
+          <br />Loojatele laekunud raha maksame välja iga kuu alguses, eelmise kuu kogutud Toetuste eest. Minimaalne väljamakse summa on 20 (kakskümmend) Eurot. Töötame selle kallal, et protsessi kiiremaks ja mugavamaks muuta.
+          <br />
+          <br />Kõikide küsimuste ja pretensioonide korral palun kirjutada
+          <a
+            href="mailto:tere@eeter.tv"
+          >tere@eeter.tv</a>.
+          Palun tutvu meie andmete töötlemise põhimõtetega
+          <a
+            href="https://www.eeter.tv/privacy"
+          >Privaatsuseeskirja</a>
+          lehel.
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,13 +71,59 @@ import AboutNav from "../components/AboutNav";
 export default {
   name: "Terms",
   components: {
-      AboutNav
+    AboutNav
   }
 };
 </script>
 
 
 <style>
+.textTitle {
+  font-size: 11pt;
+  font-family: Arial;
+  color: rgb(0, 0, 0);
+  background-color: transparent;
+  font-weight: 700;
+  font-style: normal;
+  font-variant: normal;
+  text-decoration: none;
+  vertical-align: baseline;
+  white-space: pre-wrap;
+  margin-bottom: 6px;
+}
+.textStyle {
+  font-size: 11pt;
+  font-family: Arial;
+  color: rgb(0, 0, 0);
+  background-color: transparent;
+  font-style: normal;
+  font-variant: normal;
+  text-decoration: none;
+  vertical-align: baseline;
+  white-space: pre-wrap;
+  line-height: 1.38;
+  margin-top: 0pt;
+  margin-bottom: 0pt;
+}
+
+.textSection {
+  margin-bottom: 30px;
+}
+.footer_page_container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  width: 100%;
+  font-family: "Trebuchet MS", sans-serif;
+}
+.footer_page {
+  padding: 30px 0px;
+  padding-bottom: 40px;
+  font-size: 18px !important;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
 /*! CSS Used from: https://www.eeter.tv/css/navigation.css */
 .topNavigationLink__topBar {
   border-radius: 4px;
