e9a3db72ff039cd67ef1d832b5ba3babecd59f82
Robert Unyx <robertunyx@gmail.com>
2020-04-28 15:00:02 +0300

    change

--- client/src/pages/About.vue
+++ client/src/pages/About.vue
@@ -84,7 +84,7 @@
     <p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;">
       <span
         style="font-size:11pt;font-family:Arial;color:#000000;background-color:transparent;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;white-space:pre-wrap;"
-      >Aga selleni on veel aega.
+      >Aga selleni on veel aega.</span>
     </p>
     <p>
       <br />
