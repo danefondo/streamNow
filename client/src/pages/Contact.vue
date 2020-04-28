5d5ecc7b2f0bd405e4ba988aa03ca5c0dd6e5762
Robert Unyx <robertunyx@gmail.com>
2020-04-28 16:40:14 +0300

    change

--- client/src/pages/Contact.vue
+++ client/src/pages/Contact.vue
@@ -1,5 +1,21 @@
 <template>
+  <div class="footer_page_container">
     <AboutNav />
+    <div class="footer_page">
+      <div class="textTitle">Kontakt</div>
+
+      <div class="textTitle">E-mail</div>
+      <div class="textSection">
+        <span class="textStyle">Kõikide soovide, ideede ja nõuannetega palun kirjuta meile
+          <a
+            href="mailto:tere@eeter.tv"
+          >tere@eeter.tv</a>,
+          <a href="https://www.facebook.com/Eetertv-110182260667172" target="_blank">Facebookis</a>, või
+          <a href="instagram.com/eeter.tv/" target="_blank">Instagramis</a>.
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,13 +24,59 @@ import AboutNav from "../components/AboutNav";
 export default {
   name: "Contact",
   components: {
-      AboutNav
+    AboutNav
   }
 };
 </script>
 
 
 <style>
+.textTitle {
+  font-size: 11pt;
+  font-family: Arial;
+  color: rgb(0, 0, 0);
+  background-color: transparent;
+  font-weight: 700;
+  font-style: normal;
+  font-variant: normal;
+  text-decoration: none;
+  vertical-align: baseline;
+  white-space: pre-wrap;
+  margin-bottom: 6px;
+}
+.textStyle {
+  font-size: 11pt;
+  font-family: Arial;
+  color: rgb(0, 0, 0);
+  background-color: transparent;
+  font-style: normal;
+  font-variant: normal;
+  text-decoration: none;
+  vertical-align: baseline;
+  white-space: pre-wrap;
+  line-height: 1.38;
+  margin-top: 0pt;
+  margin-bottom: 0pt;
+}
+
+.textSection {
+  margin-bottom: 30px;
+}
+
+.footer_page_container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  width: 100%;
+  font-family: "Trebuchet MS", sans-serif;
+}
+.footer_page {
+  padding: 30px 0px;
+  padding-bottom: 40px;
+  font-size: 18px !important;
+  max-width: 600px;
+  margin: 0 auto;
+}
 /*! CSS Used from: https://www.eeter.tv/css/navigation.css */
 .topNavigationLink__topBar {
   border-radius: 4px;
